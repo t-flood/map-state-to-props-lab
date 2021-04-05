5c927523a671475e82923233e4a8bcdc9bec4473
Tim Flood <tflood9@gmail.com>
2021-04-05 16:42:24 -0400

    commit tree fb5ecbe995522f75a3f9d423442bbbf4d3221ee0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
-
-import manageUsers from './reducers/manageUsers'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import manageUsers from "./reducers/manageUsers";
+import App from "./App";
 
 // add imports and code
-
+const store = createStore(
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   // add imports and code
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // add imports and code
-  ,
-  document.getElementById('root')
+  document.getElementById("root")
 );
